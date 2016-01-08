4640341cda04ae7f79c816074400ded2ad56dca4
SPARTANDROID <spartans5631@gmail.com>
2016-01-08 17:13:10 -0500

    Initial Commit

---
+++ src/org/usfirst/frc/team5631/robot/Robot.java
@@ -0,0 +1,19 @@
+
+package org.usfirst.frc.team5631.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+public class Robot extends IterativeRobot {
+    public void robotInit() {
+
+    }
+    public void autonomousPeriodic() {
+
+    }
+    public void teleopPeriodic() {
+        
+    }
+    public void testPeriodic() {
+    
+    }
+    
+}
