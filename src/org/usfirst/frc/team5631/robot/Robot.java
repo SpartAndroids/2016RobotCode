a47dc8793efee8a39852838f94f4b7ca418a836e
Nano <ymuruvi@gmail.com>
2016-01-08 18:44:30 -0500

    Added comments in preparation for tomorrow.

--- src/org/usfirst/frc/team5631/robot/Robot.java
+++ src/org/usfirst/frc/team5631/robot/Robot.java
@@ -1,17 +1,36 @@
-
+/**
+ * Spartandroids 2016Robot Code
+ * 1/9/2016
+ * This is the program which FRC team 5631 uses to control its robot.
+ */
 package org.usfirst.frc.team5631.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 public class Robot extends IterativeRobot {
+	public static Joystick mainJoystick;
+	
+	/**
+	 * Initialization method.
+	 */
     public void robotInit() {
-
+    	
     }
+    /**
+     * Constantly refreshing autonomous control method.
+     */
     public void autonomousPeriodic() {
 
     }
+    /**
+     * Constantly refreshing method, human control method.
+     */
     public void teleopPeriodic() {
         
     }
+    /**
+     * Test code method.
+     */
     public void testPeriodic() {
     
     }
